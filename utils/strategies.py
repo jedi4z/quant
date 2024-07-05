897e023a065ef9b6462f6899be02a54b14981f4b
Jesus Diaz <jesus@maypleglobal.com>
2024-07-04 23:50:12 -0300

    feat: fix strategy

--- utils/strategies.py
+++ utils/strategies.py
@@ -3,20 +3,19 @@ from utils.utilities import ccxt_ohlcv_to_df
 
 
 class BBStrategy:
-  def __init__(self, bb_len=20, n_std=20, rs_len=14, rsi_overbought=60, rsi_oversold=40) -> None:
+  def __init__(self, bb_len=20, n_std=2.0, rsi_len=14, rsi_overbought=60, rsi_oversold=40) -> None:
     self.bb_len = bb_len
     self.n_std = n_std
-    self.rs_len = rs_len
+    self.rsi_len = rsi_len
     self.rsi_overbought = rsi_overbought
     self.rsi_oversold = rsi_oversold
 
-
   def setUp(self, df):
     bb = ta.bbands(df["close"], length=self.bb_len, std=self.n_std)
-    df["bb_lowerband"] = bb.iloc[:, 0]
-    df["bb_middleband"] = bb.iloc[:, 1]
-    df["bb_upperband"] = bb.iloc[:, 2]
-    df["rsi"] = ta.rsi(df["close"], length=self.rs_len)
+    df["lbb"] = bb.iloc[:, 0]
+    df["mbb"] = bb.iloc[:, 1]
+    df["ubb"] = bb.iloc[:, 2]
+    df["rsi"] = ta.rsi(close=df["close"], length=self.rsi_len)
     self.datframe = df
 
   def checkLongSignal(self, i=None):
@@ -27,8 +26,8 @@ class BBStrategy:
 
     if (df['rsi'].iloc[i] < self.rsi_overbought) and \
         (df['rsi'].iloc[i] > self.rsi_oversold) and \
-        (df['low'].iloc[i-1] < df['bb_lowerband'].iloc[i-1]) and \
-        (df['low'].iloc[i] < df['bb_lowerband'].iloc[i]):
+        (df['low'].iloc[i-1] < df['lbb'].iloc[i-1]) and \
+        (df['low'].iloc[i] > df['lbb'].iloc[i]):
       return True
     
     return False
@@ -41,8 +40,8 @@ class BBStrategy:
 
     if (df['rsi'].iloc[i] < self.rsi_overbought) and \
         (df['rsi'].iloc[i] > self.rsi_oversold) and \
-        (df['high'].iloc[i-1] > df['bb_upperband'].iloc[i-1]) and \
-        (df['high'].iloc[i] < df['bb_upperband'].iloc[i]):
+        (df['high'].iloc[i-1] > df['ubb'].iloc[i-1]) and \
+        (df['high'].iloc[i] < df['ubb'].iloc[i]):
       return True
     
     return False
